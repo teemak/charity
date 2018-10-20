cc76e040761dfdfb8a730bcc06e2f4135f175077
baamosk <bunth667@gmail.com>
2018-10-20 01:09:04 -0400

    prototype for main view done

--- app.py
+++ app.py
@@ -11,16 +11,20 @@ def more_info():
     return render_template('more_info.html')
 
 @app.route('/help_out')
-def help_out():
+def message():
     return render_template('help_out.html')
 
 @app.route('/login')
 def login():
-    return render_template('login.html');
+    return render_template('login.html')
 
 @app.route('/account')
 def account():
     return render_template('account.html')
+
+@app.route('/post_help')
+def post_help():
+    return render_template('post_help.html')
 
 if __name__ == "__main__":
     app.run()
